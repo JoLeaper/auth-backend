57bd2494b0a018f0440c79dad44c95e12bde3edc
JoLeaper <joleap123@gmail.com>
2020-06-16 16:18:24 -0700

    all auth tests passing

--- __tests__/auth.test.js
+++ __tests__/auth.test.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const { MongoMemoryServer } = require('mongodb-memory-server');
 const mongod = new MongoMemoryServer();
 const mongoose = require('mongoose');
@@ -39,7 +41,7 @@ describe('auth routes', () => {
       });
   });
 
-  it('can create a new user', async() => {
+  it('a user can login', async() => {
     User.create({
       email: 'jjjj@jj.com',
       password: 'password1234',
@@ -47,7 +49,7 @@ describe('auth routes', () => {
     });
 
     return request(app)
-      .post('/api/v1/auth/signin')
+      .post('/api/v1/auth/login')
       .send({
         email: 'jjjj@jj.com',
         password: 'password1234',
@@ -57,6 +59,37 @@ describe('auth routes', () => {
           _id: expect.anything(),
           email: 'jjjj@jj.com',
           profileImage: 'https://usercontent2.hubstatic.com/13388733.jpg'
+        });
+      });
+  });
+
+  it('can verify if a user is logged in', async() => {
+    const user = await User.create({
+      email: 'test@test.com',
+      password: 'password1234',
+      profileImage: 'http://images.com/image.png'
+    });
+
+    // open tab in chrome
+    const agent = request.agent(app);
+
+    // on that tab login
+    return agent
+      .post('/api/v1/auth/login')
+      .send({
+        email: 'test@test.com',
+        password: 'password1234'
+      })
+      .then(() => {
+        // on the same tab verify that you are logged in
+        return agent
+          .get('/api/v1/auth/verify');
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: user.id,
+          email: 'test@test.com',
+          profileImage: 'http://images.com/image.png'
         });
       });
   });
